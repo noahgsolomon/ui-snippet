3d26c584c0535f3f3b5527e35d1f0e2022ead0d1
frija <lucydabestt@gmail.com>
2025-04-10 09:18:19 +0800

    (update): Remove blur and fix otp bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,12 @@
 'use client';
 
-import { ProgressiveBlur } from '@/components/core/progressive-blur';
 import Showcase from '@/components/sections/showcase';
 import Header from '@/components/sections/header';
 import Footer from '@/components/sections/footer';
-import { useEffect, useState } from 'react';
 import { cn } from '@/lib/utils';
 import { Toaster } from 'sonner';
 
 export default function Home() {
-  const [hideBlur, setHideBlur] = useState(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollY = window.scrollY;
-      const windowHeight = window.innerHeight;
-      const documentHeight = document.documentElement.scrollHeight;
-      setHideBlur(scrollY + windowHeight >= documentHeight);
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-
   return (
     <div
       className={cn(
@@ -33,13 +17,6 @@ export default function Home() {
       <Header />
       <Showcase />
       <Footer />
-      <ProgressiveBlur
-        className={cn(
-          'pointer-events-none fixed bottom-0 left-0 h-[60px] w-screen',
-          hideBlur ? 'opacity-0' : 'opacity-100',
-        )}
-        blurIntensity={0.2}
-      />
     </div>
   );
 }
--- components/ui/family-style-otp.tsx
+++ components/ui/family-style-otp.tsx
@@ -34,7 +34,7 @@ function AnimatedNumber({ value, placeholder }: AnimatedNumberProps) {
   );
 }
 
-function Slot(props: SlotProps & { isShaking?: boolean }) {
+function Slot(props: SlotProps & { isShaking?: boolean; }) {
   const placeholderChar = '0';
 
   return (
@@ -98,7 +98,7 @@ export default function FamilyStyleOTP() {
         otpRef.current.setSelectionRange(0, 0);
       }
 
-      setDisableSubmitButton(false);
+      // setDisableSubmitButton(false);
     }, 600);
   };
 
@@ -186,14 +186,25 @@ export default function FamilyStyleOTP() {
           handleSubmit();
         }}
         className={cn(
-          'h-[40px] w-full cursor-pointer rounded-full bg-blue-400 font-semibold text-white select-none disabled:cursor-not-allowed disabled:bg-blue-400/40',
+          'flex items-center justify-center h-[40px] w-full cursor-pointer rounded-full bg-blue-400 font-semibold text-white select-none disabled:cursor-not-allowed disabled:bg-blue-400/40',
           'transform transition-transform duration-200 ease-out hover:scale-95 active:scale-95 disabled:scale-100',
           isVerifying
             ? 'text-[#b3b3b3] disabled:bg-[#f0f0f0]'
             : 'bg-blue-400 disabled:bg-blue-400/40',
         )}
       >
-        {isVerifying ? 'Verifying' : 'Submit'}
+        {isVerifying ? (
+          <div className='w-fit flex items-center gap-1'>
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" className="animate-spin">
+              <path d="M14 8C14 8.78793 13.8448 9.56815 13.5433 10.2961C13.2417 11.0241 12.7998 11.6855 12.2426 12.2426C11.6855 12.7998 11.024 13.2418 10.2961 13.5433C9.56814 13.8448 8.78793 14 8 14C7.21206 14 6.43185 13.8448 5.70389 13.5433C4.97594 13.2418 4.31451 12.7998 3.75736 12.2426C3.2002 11.6855 2.75825 11.0241 2.45672 10.2961C2.15519 9.56815 2 8.78793 2 8C2 7.21207 2.15519 6.43186 2.45672 5.7039C2.75825 4.97595 3.2002 4.31451 3.75736 3.75736C4.31451 3.20021 4.97594 2.75825 5.7039 2.45673C6.43185 2.1552 7.21207 2 8 2C8.78793 2 9.56814 2.1552 10.2961 2.45673C11.0241 2.75826 11.6855 3.20021 12.2426 3.75736C12.7998 4.31452 13.2417 4.97595 13.5433 5.7039C13.8448 6.43186 14 7.21207 14 8L14 8Z" stroke="#DADADA" strokeWidth="3" />
+              <path d="M14 8C14 8.94687 13.7759 9.88029 13.346 10.7239C12.9162 11.5676 12.2927 12.2976 11.5267 12.8541C10.7607 13.4107 9.87381 13.778 8.9386 13.9261C8.0034 14.0743 7.04641 13.9989 6.14589 13.7063" stroke="#191919" strokeOpacity="0.36" strokeWidth="3" strokeLinecap="round" />
+            </svg>
+
+            Verifying
+          </div>
+        ) : (
+          'Submit'
+        )}
       </button>
     </div>
   );
